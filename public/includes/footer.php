43143b084068da19a4b6b453668aa1543e5555fb
danilo631 <defreitasfernandesdanilo81@gmail.com>
2025-05-05 21:50:23 -0300

    Delete public directory

--- public/includes/footer.php
+++
@@ -1,6 +0,0 @@
-<footer class="bg-dark text-white text-center py-3 mt-5">
-        <p>&copy; 2025 RetailHub. Todos os direitos reservados.</p>
-    </footer>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
